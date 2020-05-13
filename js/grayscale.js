0ff145b6f302c5ea40166443aa2a478294a4a131
YashMunjal <yashmu45@gmail.com>
2020-05-14 00:35:55 +0530

    logo

--- js/grayscale.js
+++ js/grayscale.js
@@ -30,7 +30,7 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
-      $(".navbar-brand-img").prop("src","img/TooDecimal_Black.png");
+      $(".navbar-brand-img").attr("src","img/TooDecimal_Black.png");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
     }
